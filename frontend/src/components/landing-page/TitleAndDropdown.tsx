ba84bc1b0e8b64f1c87afaebb1d4c035cd2aea6f
rakeshtds <rakeshtdsharma@yahoo.com>
2024-04-18 13:44:07 -0400

    Update TitleAndDropdown.tsx

--- frontend/src/components/landing-page/TitleAndDropdown.tsx
+++ frontend/src/components/landing-page/TitleAndDropdown.tsx
@@ -76,7 +76,7 @@ export const TitleAndDropdown = () => {
         <a href="https://www.llamaindex.ai/" target="_blank">
           <button className="flex items-center justify-center font-nunito text-lg font-bold ">
             Built for FIC LLM RAG POC
-            <img src="logo-black.svg" className="mx-2 rounded-lg" width={40} />
+            <img src="Fidelity.png" className="mx-2 rounded-lg" width={40} />
           </button>
         </a>
       </div>
